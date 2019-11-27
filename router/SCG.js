37679cfe32f117260c8332810d0cbe48492edd32
ametaldev <ametaldev@gmail.com>
2019-11-27 14:39:15 +0700

    update : project implement

--- router/SCG.js
+++ router/SCG.js
@@ -11,29 +11,36 @@ var fs = require('fs');
  * @param {object}  req     The HTTP request must have version, module.
  * @param {object}  res     The HTTP response.
  *
- * @return {json} Return the list of files or empty if error.
+ * @return {Array} Return the list value.
  */
 router.post('/cal', function (req, res) {
     let status = 400
     let json = { message: 'Bad request.' }
 
-    console.log('req : ',req.body.numData)
-    let list = req.body.numData
-    let x = 0
-    for(index in list) {
-       //x = list[index+1] - 2*((index+2)-1)
-    //    if(x > 0){
-    //     x = list[++index]  - 2*(++index - 1)
-    //    }else{
-        x = list[++index]  - 2*(++index - 1)
-    //    }
-      
-      // console.log('list[++index] ',list[++index],' 2*(++index - 1) ',2*(++index - 1))
-       console.log('X : ',x)
+    if(req.body.numData !== undefined) {
+        try{
+            let list = req.body.numData
+            
+            let indexX = list.indexOf('X')
+            let valueX = list[indexX+1] - 2*((indexX+2)-1)
+            list[indexX] = valueX
+
+            let indexY = list.indexOf('Y')
+            let valueY =list[indexY-1] + 2*((indexY+1)-1)
+            list[indexY] = valueY
+
+            let indexZ = list.indexOf('Z')
+            let valueZ = list[indexZ-1]  + 2*((indexZ+1)-1)
+            list[indexZ] = valueZ
+            
+            status = 200
+            json = {list:list}
+        }catch(err){
+            status = 500
+            json.message = err.message
+        }
     }
     
-    
-
     res.status(status).json(json)
 })
 
@@ -43,28 +50,30 @@ router.post('/cal', function (req, res) {
  * @param {object}  req     The HTTP request must have version, module.
  * @param {object}  res     The HTTP response.
  *
- * @return {json} Return the list of files or empty if error.
+ * @return {json} Return the json list place.
  */
 router.post('/getMap', function(req, res) {
     let status = 200
     let json = { message: 'Bad request.' }
 
         // call back google place api
-        // let api = `${process.env.BASE_URL}${process.env.BASE_PATH}${process.env.API_KEYWORD}${process.env.API_KEY}`
-        // axios.post(`${api}`, '').then(response => 
-        //     {
-        //       console.log("returnData : ", response.data);  
-        //       res.status(status).json(response.data) 
-        //   }).catch(error => {
-        //      // this.openDialog(this.const_dialog_error, 'Error', error, '');
-        //   });
+        let api = `${process.env.BASE_URL}${process.env.BASE_PATH}${process.env.API_KEYWORD}${process.env.API_KEY}`
+        axios.post(`${api}`, '').then(response => 
+            {
+              res.status(status).json(response.data) 
+          }).catch(error => {
+             console.log(error)
+          });
 
-        var obj;
-        fs.readFile('../placeAPI.json', 'utf8', function (err, data) {
-            if (err) throw err;
-           obj = JSON.parse(data);
-           res.status(status).json(obj)
-         });     
+          //TODO use next_page_token for next page
+
+        // Mockup data for test api
+        // var obj;
+        // fs.readFile('../placeAPI.json', 'utf8', function (err, data) {
+        //     if (err) throw err;
+        //    obj = JSON.parse(data);
+        //    res.status(status).json(obj)
+        //  });     
 })
 
 
